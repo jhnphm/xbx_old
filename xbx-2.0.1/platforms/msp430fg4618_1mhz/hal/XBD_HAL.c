6f6d29185aa8b45b3a2a4d0c8b6c8649e5e75374
John Pham <john@zeus.lan>
2011-12-01 19:39:37 -0500

    Fixed RS232

--- xbx-2.0.1/platforms/msp430fg4618_1mhz/hal/XBD_HAL.c
+++ xbx-2.0.1/platforms/msp430fg4618_1mhz/hal/XBD_HAL.c
@@ -1,10 +1,8 @@
 #include <msp430fg4618.h>   /* msp430-gcc */
-//#include "msp430F4618.h"
 #include <XBD_HAL.h>
 #include <XBD_FRW.h>
 #include <XBD_debug.h>
 #include <stdint.h>
-//#include <intrinsics.h>
 #include <signal.h>
 
 
@@ -16,152 +14,143 @@
 #define I2C_BAUDRATE 400
 #define SLAVE_ADDR 1
 
-void XBD_init()
-{
-  /* inititalisation code, called once */
+void XBD_init() {
+    WDTCTL = WDTPW+WDTHOLD;            // Stop WDT
+    /* inititalisation code, called once */
 
+    //TODO Explicitly set clock here later. Dev board defaults to 32kHz*32
+    //Default should be SMCLK=MCLK=32kHz*32
 
-  //TODO Set clock here later. Dev board defaults to 32kHz*32
-  //Default should be SMCLK=MCLK=32kHz*32
+    __disable_interrupt();
 
-  __disable_interrupt();
+    usart_init(115200);
 
-  usart_init(115200);
-  XBD_debugOut("START ATmega644 HAL\r\n");
-  XBD_debugOut("\r\n");
+    XBD_debugOut("START ATmega644 HAL\r\n");
+    XBD_debugOut("\r\n");
 
-  i2cInit();
-  i2cSetLocalDeviceAddr(SLAVE_ADDR, 0);
-  i2cSetBitrate(I2C_BAUDRATE);
-  i2cSetSlaveReceiveHandler( FRW_msgRecHand );
-  i2cSetSlaveTransmitHandler( FRW_msgTraHand );
+    i2cInit();
+    i2cSetLocalDeviceAddr(SLAVE_ADDR, 0);
+    i2cSetBitrate(I2C_BAUDRATE);
+    i2cSetSlaveReceiveHandler( FRW_msgRecHand );
+    i2cSetSlaveTransmitHandler( FRW_msgTraHand );
 
-  //Set Port 2.1 to output - Execution signal to XBH
-  P2OUT = BIT1;     // Set pin high
-  P2DIR |= BIT1;
+    //Set Port 2.1 to output - Execution signal to XBH
+    P2OUT = BIT1;     // Set pin high
+    P2DIR |= BIT1;
 
 
 }
 
 
-inline void XBD_sendExecutionStartSignal()
-{
-  /* code for output pin = on here */
-  P2OUT &= (~BIT1);
+inline void XBD_sendExecutionStartSignal() {
+    /* code for output pin = on here */
+    P2OUT &= (~BIT1);
 
 }
 
-inline void XBD_sendExecutionCompleteSignal()
-{
-  /* code for output pin = off here */
-  P2OUT |= BIT1;
+inline void XBD_sendExecutionCompleteSignal() {
+    /* code for output pin = off here */
+    P2OUT |= BIT1;
 }
 
 
-void XBD_debugOut(char *message)
-{
-  /* if you have some kind of debug interface, write message to it */
-   usart_puts(message);
+void XBD_debugOut(char *message) {
+    /* if you have some kind of debug interface, write message to it */
+    usart_puts(message);
 }
 
-void XBD_serveCommunication()
-{
-  /* read from XBD<->XBH communication channel (uart or i2c) here and call
+void XBD_serveCommunication() {
+    /* read from XBD<->XBH communication channel (uart or i2c) here and call
 
-    FRW_msgTraHand(size,transmitBuffer)
-    FRW_msgRecHand(size,transmitBuffer)
+       FRW_msgTraHand(size,transmitBuffer)
+       FRW_msgRecHand(size,transmitBuffer)
 
-    depending whether it's a read or write request
-  */
-  if(((IFG2&UCB0RXIFG)== 1) | ((IFG2&UCB0TXIFG) == 1)) {
-		twi_isr();
-	}
+       depending whether it's a read or write request
+       */
+    if(((IFG2&UCB0RXIFG)== 1) | ((IFG2&UCB0TXIFG) == 1)) {
+        twi_isr();
+    }
 }
 
 void XBD_loadStringFromConstDataArea( char *dst, const char *src  ) {
-	/*  with jtag readMem slaa149.pdf pg.31
-   		alt. if use bsl recieve block function slaa096b.pdf p.5 instead
-   		copy a zero terminated string from src (CONSTDATAAREA) to dst
-   		e.g. strcpy if CONSTDATAREA empty, strcpy_P for PROGMEM
-   	*/
-	while((*dst++ = ReadMem(F_WORD, (0x10000l) | ((uint32_t)((uint16_t)src++)))) );
+    /*  with jtag readMem slaa149.pdf pg.31
+        alt. if use bsl recieve block function slaa096b.pdf p.5 instead
+        copy a zero terminated string from src (CONSTDATAAREA) to dst
+        e.g. strcpy if CONSTDATAREA empty, strcpy_P for PROGMEM
+        */
+    while((*dst++ = ReadMem(F_WORD, (0x10000l) | ((uint32_t)((uint16_t)src++)))) );
 }
 
 
-void XBD_readPage( uint32_t pageStartAddress, uint8_t * buf )
-{
-  /* read PAGESIZE bytes from the binary buffer, index pageStartAddress
-  to buf */
-  	u16 u;
-  	for(u = 0;u < 256; ++u)			//may need to change to 128
-  	{
-  		*buf++ = ReadMem(F_WORD,pageStartAddress++);
-	}
+void XBD_readPage( uint32_t pageStartAddress, uint8_t * buf ) {
+    /* read PAGESIZE bytes from the binary buffer, index pageStartAddress
+       to buf */
+    u16 u;
+    for(u = 0;u < 256; ++u)			//may need to change to 128
+    {
+        *buf++ = ReadMem(F_WORD,pageStartAddress++);
+    }
 }
 
-void XBD_programPage( uint32_t pageStartAddress, uint8_t * buf )
-{
-  /* copy data from buf (PAGESIZE bytes) to pageStartAddress of
-  application binary */
+void XBD_programPage( uint32_t pageStartAddress, uint8_t * buf ) {
+    /* copy data from buf (PAGESIZE bytes) to pageStartAddress of
+       application binary */
 }
 
-void XBD_switchToApplication()
-{
-  /* execute the code in the binary buffer */
-  	// pointer called reboot that points to the reset vector
-  	// bootloader is located at the end of flash
-  void (*reboot)( void ) = 0x0000; // defines the function reboot to location 0x0000
-  reboot();	// calls function reboot function, did not need to change unless change location to 0x1000, at flash info memory
+void XBD_switchToApplication() {
+    /* execute the code in the binary buffer */
+    // pointer called reboot that points to the reset vector
+    // bootloader is located at the end of flash
+    void (*reboot)( void ) = 0x0000; // defines the function reboot to location 0x0000
+    reboot();	// calls function reboot function, did not need to change unless change location to 0x1000, at flash info memory
 }
 
 
-void XBD_switchToBootLoader()
-{
-  /* switch back from uploaded code to boot loader */
-  _RESET();
-//  NAKED(_reset_vector__)
-//  {
-//    /* place your startup code here */
-//    /* Make shure, the branch to main (or to your start
-//    routine) is the last line in the function */
-//    __asm__ __volatile__("br #main"::);
-//  }
+void XBD_switchToBootLoader() {
+    /* switch back from uploaded code to boot loader */
+    _RESET();
+    //  NAKED(_reset_vector__)
+    //  {
+    //    /* place your startup code here */
+    //    /* Make shure, the branch to main (or to your start
+    //    routine) is the last line in the function */
+    //    __asm__ __volatile__("br #main"::);
+    //  }
 
-//software reset (this is a soft reset equivalent on msp430 at 32ms 1Mhz smclk)
-//  wdtctl = WDT_MDLY_32; //wdt_enable(WDTO_15MS); //set watch dog WDT_MDLY_32
-//  while(1);
+    //software reset (this is a soft reset equivalent on msp430 at 32ms 1Mhz smclk)
+    //  wdtctl = WDT_MDLY_32; //wdt_enable(WDTO_15MS); //set watch dog WDT_MDLY_32
+    //  while(1);
 }
 
-uint32_t XBD_busyLoopWithTiming(uint32_t approxCycles)
-{
-  /* wait for approximatly approxCycles,
-  * then return the number of cycles actually waited */
-  /* Code from atmega_common XBD_shared_HAL.i */
-	uint32_t exactCycles=0;
-	uint16_t lastTCNT=0;
-	uint16_t overRuns=0;
+uint32_t XBD_busyLoopWithTiming(uint32_t approxCycles) {
+    /* wait for approximatly approxCycles,
+     * then return the number of cycles actually waited */
+    /* Code from atmega_common XBD_shared_HAL.i */
+    uint32_t exactCycles=0;
+    uint16_t lastTCNT=0;
+    uint16_t overRuns=0;
 
-	TBCCTL0 = TBSSEL_2 | ID_0 | MC_2;	//SMCLK, div 1, continuous, no interrupts
-	//TIMSK1 = 0;	//no interrupts
+    TBCCTL0 = TBSSEL_2 | ID_0 | MC_2;	//SMCLK, div 1, continuous, no interrupts
+    //TIMSK1 = 0;	//no interrupts
 
-	TBCCTL0 |= TBCLR;	//Clear timer register
-	XBD_sendExecutionStartSignal();
+    TBCCTL0 |= TBCLR;	//Clear timer register
+    XBD_sendExecutionStartSignal();
 
-	while(exactCycles < approxCycles)
-	{
-		if(lastTCNT > TBR)
-		{
-			++overRuns;
-		}
-		lastTCNT=TBR;
-		exactCycles = ((uint32_t)overRuns<<16)|TBR;
-	}
+    while(exactCycles < approxCycles)
+    {
+        if(lastTCNT > TBR)
+        {
+            ++overRuns;
+        }
+        lastTCNT=TBR;
+        exactCycles = ((uint32_t)overRuns<<16)|TBR;
+    }
 
-	exactCycles = ((uint32_t)overRuns<<16);
-	exactCycles |= TBR;
-	XBD_sendExecutionCompleteSignal();
+    exactCycles = ((uint32_t)overRuns<<16);
+    exactCycles |= TBR;
+    XBD_sendExecutionCompleteSignal();
 
-	return exactCycles;
+    return exactCycles;
 }
 
 #define STACK_CANARY (inv_sc?0x3A:0xC5)
@@ -183,40 +172,38 @@ uint8_t  inv_sc = 0;
 
 //extern uint8_t __stack;		/* stack top address */
 extern unsigned short _end;  ///<Last used byte of the last segment in RAM (defined by the linker)
-void XBD_paintStack(void)
-{
-  /* initialise stack measurement */
+void XBD_paintStack(void) {
+    /* initialise stack measurement */
 
-  inv_sc=!inv_sc;
+    inv_sc=!inv_sc;
 
-  //uint8_t *p = &__stack;		/* p = address of stack top */
-  //uint8_t *p = __segment_begin("CSTACK");//(void *)0x1100;     //__segment_begin("CSTACK");
-  //get address
-  //uint8_t *p_stack = __segment_end("CSTACK");
-  
-  unsigned short *address = &_end;
-  register void * __stackptr asm("r1");   ///<Access to the stack pointer
-  while(address <= (unsigned short *)__stackptr)
-  {
-    *address++ = STACK_CANARY;
-    //++p;
-  }
+    //uint8_t *p = &__stack;		/* p = address of stack top */
+    //uint8_t *p = __segment_begin("CSTACK");//(void *)0x1100;     //__segment_begin("CSTACK");
+    //get address
+    //uint8_t *p_stack = __segment_end("CSTACK");
+
+    unsigned short *address = &_end;
+    register void * __stackptr asm("r1");   ///<Access to the stack pointer
+    while(address <= (unsigned short *)__stackptr)
+    {
+        *address++ = STACK_CANARY;
+        //++p;
+    }
 }
 
-uint32_t XBD_countStack(void)
-{
-  /* return stack measurement result/0 if not supported */
-  //uint8_t *p = __segment_end("CSTACK");
-  //uint8_t *p_stack = __segment_begin("CSTACK");
-  unsigned short *address = &_end;
-  register void * __stackptr asm("r1");   ///<Access to the stack pointer
-  register uint16_t c = 0;
-  while(*address == STACK_CANARY && address <= (unsigned short *)__stackptr)
-  {
-    ++address;
-    ++c;
-  }
-  return (uint32_t)c;
+uint32_t XBD_countStack(void) {
+    /* return stack measurement result/0 if not supported */
+    //uint8_t *p = __segment_end("CSTACK");
+    //uint8_t *p_stack = __segment_begin("CSTACK");
+    unsigned short *address = &_end;
+    register void * __stackptr asm("r1");   ///<Access to the stack pointer
+    register uint16_t c = 0;
+    while(*address == STACK_CANARY && address <= (unsigned short *)__stackptr)
+    {
+        ++address;
+        ++c;
+    }
+    return (uint32_t)c;
 }
 
 #if 0
@@ -224,8 +211,8 @@ uint32_t XBD_countStack(void)
 
 int main (void)
 {
-  XBD_countStack();
-  XBD_paintStack();
+    XBD_countStack();
+    XBD_paintStack();
 
 }
 #endif
--- xbx-2.0.1/platforms/msp430fg4618_1mhz/hal/drivers/RS232.c
+++ xbx-2.0.1/platforms/msp430fg4618_1mhz/hal/drivers/RS232.c
@@ -7,89 +7,78 @@
 #include <stdint.h>
 
 #include "RS232.h"
-//#include "msp430libtypes.h"
-//#define F_CPU 10000000 // Cpu frequency (speed of SMCLK)
 
 void usart_init(uint32_t baudrate)
 {
-  // Set usart into config
-  UCA0CTL1 = UCSSEL1|UCSWRST;        // **Initialize USCI and set clock to use smclk
-  UCA0CTL0 = 0x00;			//  UCSRC to, Asyncronous 8N1
 
-  // Baud rate selection
-  // TODO Need to genercize for different F_CLK
-  UCA0BR1 = 0x09;       
-  UCA0BR0 = 0x01;
-  UCA0MCTL = 0x01<<4;
+    P2SEL |= 0x30;                           // P4.7,6 = USCI_A0 RXD/TXD
+    P2DIR |= 0x10;                          // Set port dir
+    P2DIR &= ~0x20;                         //set port dir
+    UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+    UCA0BR0 = 0x09;                           // 1MHz 115200
+    UCA0BR1 = 0x00;                           // 1MHz 115200
+    UCA0MCTL = 0x02;                          // Modulation
+    UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
 
-  //Port select
-  P4SEL |= 0x0C0;                           // P4.7,6 = USCI_A0 RXD/TXD
-
-
-  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-  //IE2_bit.UCA0RXIE = 1;       // RX Complete interrupt enabled
+    //IE2_bit.UCA0RXIE = 1;       // RX Complete interrupt enabled
 }
 
 void usart_putc(char data) {
-    while (!(UCA0TXIFG & UCA0TXIFG)); 
+    while(!(IFG2&UCA0TXIFG));
 	UCA0TXBUF = data;
 }
 
-char usart_getc(void)
-{
-    while (!(IFG2 & UCA0RXIFG));   
+char usart_getc(void) {
+    while(!(IFG2&UCA0RXIFG));
     return UCA0RXBUF;                   
 }
 
 void usart_putbyte(uint8_t val) {
-	char ch;
-	uint8_t nibble = (val & 0xf0)>>4;
-	if(nibble < 10 && nibble > 0)
-		ch='0'+nibble;
-	else if(nibble >=10)
-			ch='a'+nibble-10;
-		else
-			ch='0';
+    char ch;
+    uint8_t nibble = (val & 0xf0)>>4;
+    if(nibble < 10 && nibble > 0){
+        ch='0'+nibble;
+    }else if(nibble >=10){
+        ch='a'+nibble-10;
+    }else{
+        ch='0';
+    }
+    usart_putc(ch);
 
-	usart_putc(ch);
+    nibble = val & 0xf;
 
-
-	nibble = val & 0xf;
-
-	if(nibble < 10 && nibble > 0)
-		ch='0'+nibble;
-	else if(nibble >=10)
-			ch='a'+nibble-10;
-		else
-			ch='0';
-
-	usart_putc(ch); 
+    if(nibble < 10 && nibble > 0){
+        ch='0'+nibble;
+    }else if(nibble >=10){
+        ch='a'+nibble-10;
+    }else{
+        ch='0';
+    }
+    usart_putc(ch); 
 }
 
 void usart_puts(char *data) {
-  int len, count;
-  
-  len = strlen(data);
-  for (count = 0; count < len; count++) 
-    usart_putc(*(data+count));
+    int len, count;
+
+    len = strlen(data);
+    for (count = 0; count < len; count++) {
+        usart_putc(*(data+count));
+    }
 }
 
-void usart_putint (int usart_num)				/* Writes integer to the USART		*/
-{
- char usart_ascii_string[6]="      ";		/* Reservation to the max absvalue (2^16)/2		*/
- 
- //itoa(usart_num, usart_ascii_string, 10);	/* Convert integer to ascii [not ansi c]		*/
- sprintf(usart_ascii_string, "%d", usart_num); //sprintf <=> itoa
- usart_puts(usart_ascii_string);		/* Sens ascii pointer to the lcd_printf()		*/
+void usart_putint (int usart_num){				  /* Writes integer to the USART		*/
+    char usart_ascii_string[6]="      ";		  /* Reservation to the max absvalue (2^16)/2		*/
+
+    //itoa(usart_num, usart_ascii_string, 10);	  /* Convert integer to ascii [not ansi c]		*/
+    sprintf(usart_ascii_string, "%d", usart_num); //sprintf <=> itoa
+    usart_puts(usart_ascii_string);		          /* Sens ascii pointer to the lcd_printf()		*/
 }
 
-void usart_putu32hex(u32 num)
-{
-	char s[10];
-	//usart_puts( ltoa(num,s,16) );
-        sprintf(s,"%lX",num);
-        usart_puts(s);
-
+void usart_putu32hex(u32 num) {
+    char s[10];
+    //usart_puts( ltoa(num,s,16) );
+    sprintf(s,"%lX",num);
+    usart_puts(s);
 }
 
 
